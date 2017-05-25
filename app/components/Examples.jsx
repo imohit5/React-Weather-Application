8468ab6fa05a471b451ac4a0a6fb87035ec0475e
Mohit Arun Inampudi <mi9.developer@gmail.com>
2017-05-24 20:43:07 -0400

    updated examples and main page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,20 @@
 var React = require('react');
-
+var {Link} = require('react-router')
 
 var Examples = (props) => {
   return(
-    <h3>Examples Component!</h3>
+    <div>
+      <h1 className='text-center'>Examples</h1>
+      <p> How this Weather application works</p>
+      <ol>
+        <li>
+          <Link to='/?location=Atlanta'>Atlanta, GA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Vijayawada'>Vijayawada, India</Link>
+        </li>
+      </ol>
+    </div>
   );
 }
 
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -6,8 +6,17 @@ var Main = (props) => {
   return (
     <div>
       <Nav/>
-      <h2>Main Component</h2>
-      {props.children}
+      <div className="row">
+
+        <div className="columns small-centered medium-6 large-4">
+          {props.children}
+        </div>
+
+
+
+      </div>
+
+
     </div>
   );
 }
